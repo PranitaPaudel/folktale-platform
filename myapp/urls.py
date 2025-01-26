bb6dc58eea21303a294a2209b9c15a39d15594c0
PranitaPaudel <pranitapaudel67@gmail.com>
2025-01-26 18:29:25 +0545

    Changed function-based view into class-based views

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,14 +1,12 @@
 from django.urls import path
-from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import Index, Form, Detail, Tale, NewsletterSignup
 
 urlpatterns = [
-    path('', views.index, name="homepage"),
-    # path('', views.index, name="homepage"),
-    path('form/', views.form, name="folkForm"),
-    # path('tale/', views.tale, name='tale'),
-    path('tale/<int:post_id>/', views.tale, name='tale'),
-    path('detail/', views.detail, name="detail"),
-    path('newsletter/', views.newsletter_signup, name='newsletter_signup'),
+    path('', Index.as_view(), name="homepage"),
+    path('form/', Form.as_view(), name = "folkForm"),
+    path('tale/<int:post_id>/', Tale.as_view(), name='tale'),
+    path('detail/', Detail.as_view(), name="detail"),
+    path('newsletter/', NewsletterSignup.as_view(), name='newsletter_signup'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- myapp/views.py
+++ myapp/views.py
@@ -1,21 +1,22 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Post
-from django.shortcuts import render, get_object_or_404
-from django.contrib import messages
 from .forms import NewsletterSignupForm
 from django.http import HttpResponse
 from django.db import IntegrityError
-from django.contrib import messages
+from django.views import View
 
-def index(request):
-    context = {
+class Index(View):
+    def get(self, request):
+        context = {
         'posts':Post.objects.all()
-    }
-    return render(request, 'myapp/index.html', context)
+        }
+        return render(request, 'myapp/index.html', context)
 
-def form(request):
-    if request.method == 'POST':
-        # Process form submission
+class Form(View):
+    def get(self, request):
+        return render(request, 'myapp/form.html')
+    
+    def post(self, request):
         title = request.POST.get('title')
         content = request.POST.get('description')
         image = request.FILES.get('image')
@@ -27,60 +28,25 @@ def form(request):
             image=image,
             author=author
         )
-        return redirect('homepage')  # Redirect after successful submission
-    return render(request, 'myapp/form.html')
+        return redirect('homepage') 
 
-# def tale(request):
-#     context = {
-#         'posts':Post.objects.all()
-#     }
-#     return render(request, 'myapp/tale.html', context)
-def tale(request, post_id):  # Change post_id to pk
-    post = get_object_or_404(Post, id=post_id)
-    return render(request, 'myapp/tale.html', {'post': post})
+class Tale(View):
+    def get(self, request, post_id):
+        post = get_object_or_404(Post, id=post_id)
+        return render(request, 'myapp/tale.html', {'post': post})
 
-def detail(request):
-    query = request.GET.get('search', '')
-    posts = Post.objects.filter(title__icontains=query) if query else Post.objects.all()
-    context = {
-        'posts': posts,
-        'search_query': query
-    }
-    return render(request, 'myapp/detail.html', context)
+class Detail(View):
+    def get(self, request):
+        query = request.GET.get('search', '')
+        posts = Post.objects.filter(title__icontains=query) if query else Post.objects.all()
+        context = {
+            'posts': posts,
+            'search_query': query
+        }
+        return render(request, 'myapp/detail.html', context)
 
-# def newsletter_signup(request):
-#     if request.method == 'POST':
-#         form = NewsletterSignupForm(request.POST)
-#         if form.is_valid():
-#             try:
-#                 form.save()
-#                 messages.success(request, 'Successfully subscribed to our newsletter!')
-#                 return HttpResponse('''
-#                     <script>
-#                         alert('Successfully subscribed to our newsletter!');
-#                         window.location.href = '/';
-#                     </script>
-#                 ''')
-#             except IntegrityError:
-#                 messages.error(request, 'This email is already subscribed.')
-#                 return HttpResponse('''
-#                     <script>
-#                         alert('This email is already subscribed.');
-#                         window.location.href = '/';
-#                     </script>
-#                 ''')
-#         else:
-#             messages.error(request, 'Please enter a valid email address.')
-#             return HttpResponse('''
-#                 <script>
-#                     alert('Please enter a valid email address.');
-#                     window.location.href = '/';
-#                 </script>
-#             ''')
-#     return redirect('homepage')
-
-def newsletter_signup(request):
-    if request.method == 'POST':
+class NewsletterSignup(View):
+    def post(self, request):
         form = NewsletterSignupForm(request.POST)
         if form.is_valid():
             try:
@@ -105,4 +71,6 @@ def newsletter_signup(request):
                     window.location.href = '{}';
                 </script>
             '''.format(request.META.get('HTTP_REFERER', '/')))
-    return redirect('homepage')
+
+    def get(self, request):
+        return redirect('homepage')
